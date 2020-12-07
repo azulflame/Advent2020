f919dcc42927fe5a46b001c810f5bd404aea1cae
Todd Bensmiller <toddbjta@gmail.com>
2020-12-07 01:08:37 -0600

    day 7

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,53 @@
+package me.toddbensmiller.advent
+
+/*
+ * Created by Todd on 12/6/2020.
+ */
+fun day7()
+{
+	println(day7part1(ListHolder.day7))
+	println(day7part2(ListHolder.day7))
+}
+
+fun day7part1(list: List<String>): Int
+{
+	val canContainShinyGold: HashSet<String> = HashSet()
+	canContainShinyGold.add("shiny gold")
+	var changed = true
+	val firstMap = list.map {
+		it.split(Regex(" bags contain "))[0] to Regex("[0-9]+ ([a-z ]+) bag")
+			.findAll(it.split(Regex(" bags contain "))[1]).map { a -> a.groupValues.drop(1).first()}.toList()}.toMap()
+	while (changed)
+	{
+		val startSize = canContainShinyGold.size
+		canContainShinyGold.addAll(firstMap.filter { x -> canContainShinyGold.intersect(x.value).isNotEmpty() }.map { it.key})
+		changed = startSize < canContainShinyGold.size
+	}
+
+	return canContainShinyGold.size -1
+}
+
+fun day7part2(list: List<String>): Int
+{
+	val firstMap = list.map {
+		it.split(Regex(" bags contain "))[0] to Regex("([0-9]+ [a-z ]+) bag")
+			.findAll(it.split(Regex(" bags contain "))[1]).map { a -> a.groupValues.drop(1).first()}.toList()}.toMap()
+
+
+	return dfs(firstMap, "shiny gold")
+}
+
+fun dfs(graph: Map<String, List<String>>, target: String): Int
+{
+	if(graph[target].isNullOrEmpty())
+		return 0
+	var sum = 0
+	for(x in graph[target]!!)
+	{
+		val mult = Regex("([0-9]+)").find(x)!!.value.toInt()
+		val resultSet = Regex(" ([a-z ]+)").find(x)
+		val bagName = resultSet!!.groupValues[1]
+		sum += mult * (dfs(graph, bagName)+1)
+	}
+	return sum
+}
