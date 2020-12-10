845802773d73a85a51c9c97aa30592b699e679d0
Todd Bensmiller <toddbjta@gmail.com>
2020-12-10 00:16:28 -0600

    added linear sort, autoformat

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,5 +1,7 @@
 package me.toddbensmiller.advent
 
+import kotlin.system.measureNanoTime
+
 /*
  * Created by Todd on 12/9/2020.
  */
@@ -7,35 +9,41 @@ fun day10(): Pair<Number, Number>
 {
 	return Pair(day10part1(ListHolder.day10), day10part2(ListHolder.day10))
 }
+
 fun day10part1(input: List<String>): Number
 {
-	val inMap = input.map { it.toInt()}.plus(0).sorted().plus(input.map { it.toInt()}.maxOf {it}+3)
+	val inMap = d10Sort(input.map { it.toInt() }.plus(0))
 	val reduced: HashMap<Int, Int> = HashMap()
-	for(x in 0 until inMap.size-1)
+	for (x in 0 until inMap.size - 1)
 	{
-		val diff = inMap[x+1]-inMap[x]
-		reduced[diff] = (reduced[diff] ?: 0)+1
+		val diff = inMap[x + 1] - inMap[x]
+		reduced[diff] = (reduced[diff] ?: 0) + 1
 	}
 	return reduced[3]!! * reduced[1]!!
 }
+
 fun day10part2(input: List<String>): Number
 {
-	return check10Cache(input.map { it.toInt()}.plus(input.map { it.toInt()}.maxOf {it}+3).sorted(), 0)
+	val t: Number
+	println(measureNanoTime {
+		t = check10Cache(input.map { it.toInt() }.plus(input.map { it.toInt() }.maxOf { it } + 3), 0)
+	})
+	return t
 }
 
-fun check10Cache(input: List<Int>, wallVal: Int): Long
+fun check10Cache(input: List<Int>, start: Int): Long
 {
-	if(D10.hm.contains(wallVal))
-		return D10.hm[wallVal]!!
+	if (D10.hm.contains(start))
+		return D10.hm[start]!!
 	var count = 0L
-	if(wallVal == input.last())
+	if (start == input.last())
 	{
 		count = 1
 	}
-	for(x in 1..3)
+	for (x in 1..3)
 	{
-		val toTest = wallVal + x
-		if(input.contains(toTest))
+		val toTest = start + x
+		if (input.contains(toTest))
 		{
 			val t = check10Cache(input, toTest)
 			D10.hm[toTest] = t
@@ -45,6 +53,25 @@ fun check10Cache(input: List<Int>, wallVal: Int): Long
 	return count
 }
 
-object D10 {
+object D10
+{
 	val hm: HashMap<Int, Long> = HashMap()
+}
+
+fun d10Sort(input: List<Int>): List<Int>
+{
+	val validNums = Array(input.size * 3) { false }
+	val mList: MutableList<Int> = mutableListOf()
+	for (x in input)
+	{
+		validNums[x] = true
+	}
+	for (x in validNums.indices)
+	{
+		if (validNums[x])
+		{
+			mList.add(x)
+		}
+	}
+	return mList.plus(mList.last() + 3)
 }
